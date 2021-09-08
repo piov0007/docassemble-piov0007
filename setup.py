1aa28749b5e52aeaa192f75f7f9b70048afd8acc
Robert Piovesan <88375199+piov0007@users.noreply.github.com>
2021-09-08 16:12:18 +0930

    Split into main & question .yml files

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.piov0007',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.piov0007\n\nA docassemble extension.\n\n## Author\n\nRobert Piovesan, piov0007@flinders.edu.au\n\n',
+      long_description='# docassemble.piov0007\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nRobert Piovesan, piov0007@flinders.edu.au\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Robert Piovesan',
       author_email='piov0007@flinders.edu.au',
